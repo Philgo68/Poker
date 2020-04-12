04fc2e3995346a98fedf322b36b49b6b14880575
Philgo <philgo68@gmail.com>
2020-04-11 19:03:44 -0700

    Add Seven Card Hand and Layouts for 6/7 Cards.  Tightened margins on the 2-5 card hands.

---
+++ Poker/Models/SevenCardHand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Poker.Models
+{
+  public class SevenCardHand : BaseHand
+  {
+    public override string Name { get { return "Seven Card"; } }
+    public override byte CardCount { get { return 7; } }
+    public SevenCardHand() : base() { }
+    public SevenCardHand(ulong cardsMask) : base(cardsMask) { }
+    public SevenCardHand(string cards) : base(cards) { }
+
+    public override (int, uint) Evaluate(ulong board, ulong filler = 0x0UL)
+    {
+      return base.PokerEvaluate(board, filler);
+    }
+  }
+}
